10850c8f408c2f5a4f9ccb9774b3ec66774bde69
Jayden E. <aneeshedara@gmail.com>
2024-10-11 01:30:40 -0700

    Finish initialization

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -12,13 +12,12 @@
 #include <chrono>
 #include <thread>
 
-VulkanEngine* loadedEngine = nullptr;
+VulkanEngine *loadedEngine = nullptr;
 
-constexpr bool bUseValidationLayers = false;
+constexpr bool bUseValidationLayers = true;
 
-VulkanEngine& VulkanEngine::Get() { return *loadedEngine; }
-void VulkanEngine::init()
-{
+VulkanEngine &VulkanEngine::Get() { return *loadedEngine; }
+void VulkanEngine::init() {
     // only one engine initialization is allowed with the application.
     assert(loadedEngine == nullptr);
     loadedEngine = this;
@@ -26,15 +25,15 @@ void VulkanEngine::init()
     // We initialize SDL and create a window with it.
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN);
+    SDL_WindowFlags window_flags = (SDL_WindowFlags) (SDL_WINDOW_VULKAN);
 
     _window = SDL_CreateWindow(
-        "Vulkan Engine",
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
-        _windowExtent.width,
-        _windowExtent.height,
-        window_flags);
+            "Vulkan Engine",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            _windowExtent.width,
+            _windowExtent.height,
+            window_flags);
 
     init_vulkan();
 
@@ -52,29 +51,58 @@ void VulkanEngine::init_vulkan() {
     vkb::InstanceBuilder builder;
 
     auto inst_ret = builder.set_app_name("Vulkan Application")
-        .request_validation_layers(bUseValidationLayers)
-        .use_default_debug_messenger()
-        .require_api_version(1, 3, 0)
-        .build();
+                            .request_validation_layers(bUseValidationLayers)
+                            .use_default_debug_messenger()
+                            .require_api_version(1, 3, 0)
+                            .build();
 
     vkb::Instance vkb_inst = inst_ret.value();
 
-    _instance = vkb_inst.instance;
+    _instance        = vkb_inst.instance;
     _debug_messenger = vkb_inst.debug_messenger;
 
-    SDL_Vulkan_CreateSurface()
+    SDL_Vulkan_CreateSurface(_window, _instance, &_surface);
+
+    // 1.3 Features
+    VkPhysicalDeviceVulkan13Features vk13features{
+            .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES};
+    vk13features.dynamicRendering = true;
+    vk13features.synchronization2 = true;
+
+    VkPhysicalDeviceVulkan12Features vk12features{
+            .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES};
+    vk12features.bufferDeviceAddress = true;// Allows for use of GPU pointers w/o binding buffers
+    vk12features.descriptorIndexing  = true;// Bind-less textures
+
+    // Get GPU that supports our requested features
+    vkb::PhysicalDeviceSelector selector{vkb_inst};
+    vkb::PhysicalDevice physicalDevice = selector
+                                                 .set_minimum_version(1, 3)
+                                                 .set_required_features_13(vk13features)
+                                                 .set_required_features_12(vk12features)
+                                                 .set_surface(_surface)
+                                                 .select()
+                                                 .value();
+
+    vkb::DeviceBuilder deviceBUilder{physicalDevice};
+    vkb::Device vkbDevice = deviceBUilder.build().value();
+
+    _device    = vkbDevice.device;
+    _chosenGPU = physicalDevice.physical_device;
 }
 
-void VulkanEngine::init_swapchain() {  }
+void VulkanEngine::init_commands() {}
 
-void VulkanEngine::init_commands() {  }
+void VulkanEngine::init_sync_structures() {}
 
-void VulkanEngine::init_sync_structures() {  }
-
-void VulkanEngine::cleanup()
-{
+void VulkanEngine::cleanup() {
     if (_isInitialized) {
+        destroy_swapchain();
 
+        vkDestroySurfaceKHR(_instance, _surface, nullptr);
+        vkDestroyDevice(_device, nullptr);
+        vkb::destroy_debug_utils_messenger(_instance, _debug_messenger);
+        vkDestroyInstance(_instance, nullptr);
         SDL_DestroyWindow(_window);
     }
 
@@ -82,13 +110,11 @@ void VulkanEngine::cleanup()
     loadedEngine = nullptr;
 }
 
-void VulkanEngine::draw()
-{
+void VulkanEngine::draw() {
     // nothing yet
 }
 
-void VulkanEngine::run()
-{
+void VulkanEngine::run() {
     SDL_Event e;
     bool bQuit = false;
 
@@ -118,5 +144,33 @@ void VulkanEngine::run()
         }
 
         draw();
+    }
+}
+void VulkanEngine::create_swapchain(uint32_t width, uint32_t height) {
+    vkb::SwapchainBuilder swapchainBuilder{_chosenGPU, _device, _surface};
+    _swapchainImageFormat = VK_FORMAT_B8G8R8A8_UNORM;
+
+    vkb::Swapchain vkbSwapchain = swapchainBuilder
+                                          .set_desired_format(VkSurfaceFormatKHR{.format = _swapchainImageFormat, .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR})
+                                          .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)// Hard VSync
+                                          .set_desired_extent(width, height)
+                                          .add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+                                          .build()
+                                          .value();
+
+    _swapchainExtent     = vkbSwapchain.extent;
+    _swapchain           = vkbSwapchain.swapchain;
+    _swapchainImages     = vkbSwapchain.get_images().value();
+    _swapchainImageViews = vkbSwapchain.get_image_views().value();
+}
+
+void VulkanEngine::init_swapchain() {
+    create_swapchain(_windowExtent.width, _windowExtent.height);
+}
+
+void VulkanEngine::destroy_swapchain() {
+    vkDestroySwapchainKHR(_device, _swapchain, nullptr);
+    for (int i = 0; i < _swapchainImageViews.size(); ++i) {
+        vkDestroyImageView(_device, _swapchainImageViews[i], nullptr);
     }
 }
--- src/vk_engine.h
+++ src/vk_engine.h
@@ -7,37 +7,47 @@
 
 class VulkanEngine {
 public:
+    bool _isInitialized{false};
+    int _frameNumber{0};
+    bool stop_rendering{false};
+    VkExtent2D _windowExtent{1700, 900};
 
-	bool _isInitialized{ false };
-	int _frameNumber {0};
-	bool stop_rendering{ false };
-	VkExtent2D _windowExtent{ 1700 , 900 };
+    struct SDL_Window *_window{nullptr};
 
-	struct SDL_Window* _window{ nullptr };
+    static VulkanEngine &Get();
 
-	static VulkanEngine& Get();
+    //initializes everything in the engine
+    void init();
 
-	//initializes everything in the engine
-	void init();
+    //shuts down the engine
+    void cleanup();
 
-	//shuts down the engine
-	void cleanup();
+    //draw loop
+    void draw();
 
-	//draw loop
-	void draw();
+    //run main loop
+    void run();
 
-	//run main loop
-	void run();
+    VkInstance _instance;                     // Vulkan API instance
+    VkDebugUtilsMessengerEXT _debug_messenger;// Debug messenger
+    VkPhysicalDevice _chosenGPU;              // GPU
+    VkDevice _device;                         // Logical device
+    VkSurfaceKHR _surface;                    // Window
 
-	VkInstance _instance;                      // Vulkan API instance
-	VkDebugUtilsMessengerEXT _debug_messenger; // Debug messenger
-	VkPhysicalDevice _chosenGPU;		   // GPU
-	VkDevice _device;			   // Logical device
-	VkSurfaceKHR _surface;			   // Window
+    // Swapchain
+    VkSwapchainKHR _swapchain;
+    VkFormat _swapchainImageFormat;
+
+    std::vector<VkImage> _swapchainImages;        // Handle to actual image object
+    std::vector<VkImageView> _swapchainImageViews;// Wrapper to perform actions on the image
+    VkExtent2D _swapchainExtent;
 
 private:
-	void init_vulkan();
-	void init_swapchain();
-	void init_commands();
-	void init_sync_structures();
+    void init_vulkan();
+    void init_swapchain();
+    void init_commands();
+    void init_sync_structures();
+
+    void create_swapchain(uint32_t width, uint32_t height);
+    void destroy_swapchain();
 };
